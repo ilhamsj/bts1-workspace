64f563262c8f3e15adb57672388d96d467f6696e
ilhamsj <ilhamsaputrajati@gmail.com>
2025-07-06 19:45:50 +0700

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+®Since 2010 - Founded at 06/2025
+
+Austin – Texas ->
+Villa Kuta Indah,
+Citra Maja City
+
+Years Educating -> +32 Hours
+Happy Children -> +10 Happy children
---
+++ workspace/scrap.sh
@@ -0,0 +1,12 @@
+wget --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --restrict-file-names=windows \
+     --span-hosts \
+     --domains aldnoahzerousa \
+     --no-parent \
+     --directory-prefix=joy \
+     --user-agent="Mozilla/5.0" \
+     https://www.aldnoahzerousa.com
